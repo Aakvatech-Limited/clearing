bd6fdef6f57a80617d4c5b74f63f05d0a8e022c8
Sydney Kibanga <skibanga@aakvatech.com>
2024-10-18 10:39:25 +0300

    fix: set proper status for the clearing_file_doc

--- clearing/clearing/doctype/cf_delivery_note/cf_delivery_note.py
+++ clearing/clearing/doctype/cf_delivery_note/cf_delivery_note.py
@@ -5,12 +5,17 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 
+
 class CFDeliveryNote(Document):
-    
+
     def before_submit(self):
         # Check if the Delivery Note has a linked Clearing File
         if not self.clearing_file:
-            frappe.throw(_("This Delivery Note is not linked to a Clearing File. Please link a Clearing File before submitting."))
+            frappe.throw(
+                _(
+                    "This Delivery Note is not linked to a Clearing File. Please link a Clearing File before submitting."
+                )
+            )
 
         # Get the linked Clearing File document
         clearing_file_doc = frappe.get_doc("Clearing File", self.clearing_file)
@@ -18,7 +23,12 @@ class CFDeliveryNote(Document):
         # Check if the Clearing File status is 'Cleared'
         if clearing_file_doc.status == "Cleared":
             # Update the status of the Delivery Note to 'Delivered'
-            self.status = "Delivered"
+            clearing_file_doc.status = "Delivered"
+            clearing_file_doc.save()
         else:
             # Raise an error if the Clearing File is not cleared
-            frappe.throw(_("The linked Clearing File {0} is not cleared. You must clear it before submitting the Delivery Note.").format(self.clearing_file))
+            frappe.throw(
+                _(
+                    "The linked Clearing File {0} is not cleared. You must clear it before submitting the Delivery Note."
+                ).format(self.clearing_file)
+            )

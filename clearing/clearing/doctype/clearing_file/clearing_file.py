c9de2f899a1913621d38d95047a50f318dbaeaf3
Sydney Kibanga <skibanga@aakvatech.com>
2024-10-18 15:50:05 +0300

    feat: Validate Clearing Charges only if related doctype totals are non-zero

--- clearing/clearing/doctype/clearing_file/clearing_file.py
+++ clearing/clearing/doctype/clearing_file/clearing_file.py
@@ -1,6 +1,5 @@
 import frappe
 from frappe.model.document import Document
-from frappe.contacts.doctype.address.address import get_address_display
 from frappe import _
 
 
@@ -12,8 +11,9 @@ class ClearingFile(Document):
     def before_submit(self):
         # On submit, enforce that all required documents are attached
         ensure_all_documents_attached(self, "clearing_file_document")
-        # if self.status != "Delivered":
-        #     frappe.throw(_("You can't Submit if Clearing file status is not Delivered"))
+
+        # Check if clearing charges exist and validate the amounts only if relevant doctypes have total charges
+        self.check_and_validate_clearing_charges()
 
     def check_and_update_status(self):
         # Required fields for "Pre-Lodged" status
@@ -36,6 +36,107 @@ class ClearingFile(Document):
                 self.status = "Pre-Lodged"
         else:
             pass
+
+    def check_and_validate_clearing_charges(self):
+        # Fetch the total charges from each related doctype
+        related_doctypes = [
+            {
+                "doctype": "TRA Clearance",
+                "charge_type": "TRA Clearance",
+                "field": "total_charges",
+            },
+            {
+                "doctype": "Port Clearance",
+                "charge_type": "Port Clearance",
+                "field": "total_charges",
+            },
+            {
+                "doctype": "Shipment Clearance",
+                "charge_type": "Shipment Clearance",
+                "field": "total_charges",
+            },
+            {
+                "doctype": "Physical Verification",
+                "charge_type": "Physical Verification",
+                "field": "total_charges",
+            },
+        ]
+
+        charges_needed = False
+
+        # Loop through each related doctype and check if it has total charges > 0
+        for doc_info in related_doctypes:
+            related_docs = frappe.get_all(
+                doc_info["doctype"],
+                filters={"clearing_file": self.name},
+                fields=[doc_info["field"]],
+            )
+
+            if related_docs:
+                total_charges = related_docs[0][doc_info["field"]]
+
+                # If there are total charges, we need to validate clearing charges
+                if total_charges > 0:
+                    charges_needed = True
+
+                    # Ensure clearing charges exist
+                    clearing_charges = frappe.get_all(
+                        "Clearing Charges",
+                        filters={"clearing_file": self.name},
+                        fields=["name"],
+                    )
+
+                    if not clearing_charges:
+                        frappe.throw(
+                            _(
+                                "No Clearing Charges have been created for this Clearing File. Please create the charges before submitting."
+                            )
+                        )
+
+                    # Get the clearing charge document
+                    clearing_charge_doc = frappe.get_doc(
+                        "Clearing Charges", clearing_charges[0].name
+                    )
+
+                    # Check if the corresponding charge type exists in Clearing Charges
+                    matching_charge = next(
+                        (
+                            charge
+                            for charge in clearing_charge_doc.charges
+                            if charge.charge_type == doc_info["charge_type"]
+                        ),
+                        None,
+                    )
+
+                    if not matching_charge:
+                        frappe.throw(
+                            _(
+                                "Clearing Charges entry for {0} is missing. Please create the corresponding charge."
+                            ).format(doc_info["charge_type"])
+                        )
+
+                    # Check if the amount matches
+                    if matching_charge.amount != total_charges:
+                        frappe.throw(
+                            _(
+                                "The amount for {0} does not match the expected value. Expected: {1}, Found: {2}."
+                            ).format(
+                                doc_info["charge_type"],
+                                total_charges,
+                                matching_charge.amount,
+                            )
+                        )
+
+        # If any of the related Doctypes has total charges, ensure that corresponding clearing charges are created
+        if charges_needed:
+            pass
+        else:
+            # If no charges are needed, allow the submission without further validation
+            frappe.msgprint(
+                _(
+                    "No related charges found in the related doctypes, submission allowed."
+                )
+            )
 
 
 def ensure_all_documents_attached(self, type):

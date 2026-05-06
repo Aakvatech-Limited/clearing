24d914f1ea9488dcdfcc722644b7e8f6936ff813
Emanuel Kagombora <emanuelkagombora28@gmail.com>
2026-05-06 14:44:12 +0300

    feat(container interchange): require Final EIR when CF Delivery Note is linked

--- clearing/clearing/doctype/container_interchange/container_interchange.py
+++ clearing/clearing/doctype/container_interchange/container_interchange.py
@@ -10,6 +10,25 @@ from frappe.utils import flt
 class ContainerInterchange(Document):
 	def before_submit(self):
 		self.validate_shipping_line_deposit()
+		self.validate_final_eir_against_delivery_note()
+
+	def validate_final_eir_against_delivery_note(self):
+		if not self.clearing_file:
+			return
+
+		has_linked_delivery_note = bool(
+			frappe.db.exists(
+				"CF Delivery Note",
+				{"container_interchange": self.name, "docstatus": ["<", 2]},
+			)
+		)
+		if not has_linked_delivery_note:
+			return
+
+		if not self.final or not self.eir_number_reference or not self.eir_date:
+			frappe.throw(
+				_("Final EIR is required (Has Final EIR, EIR Number Reference, EIR Date) before submitting.")
+			)
 
 	def validate_shipping_line_deposit(self):
 		if not self.clearing_file:
